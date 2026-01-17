50743fbec8cf21f3302c8970f9a43d04abe9a71b
akirst <platipuskumar@gmail.com>
2026-01-17 18:35:37 +0530

    Update Data Loader.py

--- Data Loader.py
+++ Data Loader.py
@@ -5,3 +5,9 @@ def build_multiscale_inputs(x):
     mid   = x[:, -24::2]                     # downsample (10 min)
     long  = x[:, -48::4]                     # downsample (20 min)
     return short, mid, long
+
+
+
+# a time step is 5 min so 12 previous time steps = 1 hour 
+# when we take the data of past of 4 hours that is the last loop we take alternate 4th value so that window match
+# the key is that now the model knows that short term momory and long term too
